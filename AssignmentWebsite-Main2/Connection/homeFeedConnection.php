179d9cc0c7503a88d946d451552999d981df863c
Daniel Pearson <danpearson1701@gmail.com>
2024-05-03 13:05:36 +0100

    c

--- AssignmentWebsite-Main2/Connection/homeFeedConnection.php
+++ AssignmentWebsite-Main2/Connection/homeFeedConnection.php
@@ -1,13 +1,36 @@
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "postInfo";
-$conn1 = new mysqli($servername, $username, $password, $database);
+$database = "postinfo";
 
-if($conn1->connect_error){
-    die("Connection Failed". $conn->connect_error);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-return $conn1;
+// SQL to check if the table exists
+$tableName = "postsdata";
+$sql = "SHOW TABLES LIKE '$tableName'";
+$result = $conn->query($sql);
+
+// If table does not exist, create it
+if ($result->num_rows == 0) {
+    $createTableSql = "CREATE TABLE $tableName (
+        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        userID INT(11),
+        usersName VARCHAR(255) NOT NULL,
+        textVal VARCHAR(300) NOT NULL,
+        postType VARCHAR(155) NOT NULL,
+        postImage BLOB,
+        profilePic BLOB
+    )";
+} else {
+    echo "Table $tableName already exists";
+}
+
+$conn->close();
+
